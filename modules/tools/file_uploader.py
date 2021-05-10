04806b558961066ee28abb5876de20b6a2ffb7ce
Yanis <59359557+yanis-man@users.noreply.github.com>
2021-05-10 22:25:47 +0200

    minor update
    api url has been moved to the config file

--- modules/tools/file_uploader.py
+++ modules/tools/file_uploader.py
@@ -9,7 +9,7 @@ cfg = FileConfig()
 
 def upload_image(path):
     with open(path, "rb") as filee:
-        url = "https://api.imgbb.com/1/upload"
+        url = cfg.API_URL
         payload = {
             "key" : cfg.API_KEY,
             "image": base64.b64encode(filee.read()),
